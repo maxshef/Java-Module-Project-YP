a82a0f776dc5bbf69df81e33fc4cc219912f725f
minishev <minishev-ma777@yandex.ru>
2023-05-13 10:50:54 +0300

    И пусть теперь все получиться!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,62 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ProductFeatures productFeatures = new ProductFeatures (); //объект класса ProductFeatures
+        System.out.println("Сколько счетов у Вас будет?");
+        int howPeople = 0;
+        while (true){
+            howPeople = scanner.nextInt();
+            if (howPeople <=1) {
+                System.out.println("Введено некорректное значение для подсчёта, введите число больше 1");
+            }
+            if(howPeople > 1) {
+                System.out.println("Счет будет разделен на:" + howPeople);
+                break;
+            }
+        }
+        productFeatures.product(scanner);
+        // - Фарматерр:
+        double finalScorePeople = productFeatures.finalProductPrice/howPeople;
+        String RUB;
+        String RUB2;
+        int endingRubles = (int) productFeatures.finalProductPrice;// % 100 / 10;
+        int endingRubles2 = (int) finalScorePeople;// % 100 / 10;
+        if (endingRubles % 100 >= 11 && endingRubles % 100 <= 14) {
+            RUB = "рублей";
+        } else
+
+            switch (endingRubles %10){
+                case 1:
+                    RUB = "рубль";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    RUB = "рубля";
+                    break;
+                default:
+                    RUB = "рублей";
+                    break;
+            }
+        if (endingRubles2 % 100 >= 11 && endingRubles2 % 100 <= 14) {
+            RUB2 = "рублей";
+        } else
+            switch (endingRubles2 % 10){
+                case 1:
+                    RUB2 = "рублю";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    RUB2 = "рубля";
+                    break;
+                default:
+                    RUB2 = "рублей";
+                    break;
+            }
+        System.out.println("Список продуктов:\n"+productFeatures.listProducts+"\n");
+        String format = "Общая стоимость составила:  %.2f "+RUB+ "\nна каждого по: %.2f " +RUB2+".";
+        System.out.println(String.format(format, productFeatures.finalProductPrice, finalScorePeople));
     }
 }
---
+++ src/main/java/ProductFeatures.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ProductFeatures {
+    double finalProductPrice = 00.00;
+    double productPrice = 00.00;
+    String listProducts = "";
+
+    public void product(Scanner scanner) {
+        while (true) {
+            System.out.println("Введите название товара");
+            String product = scanner.next();
+            listProducts += product + "\n";
+            System.out.println("Введите цену товара в формате ХХ,ХХ");
+            double productPrice = scanner.nextDouble();
+            if (productPrice <= 0) {
+                System.out.println("Введена неверная цена, цена должна быть больше 0.00");
+            }
+            if (productPrice > 0) {
+            }
+            System.out.println("Хотите продолжить ввод товаров или введите завершить");
+            finalProductPrice = finalProductPrice + productPrice;
+            String complete = scanner.next();
+            if (complete.equalsIgnoreCase("Завершить")) {
+                System.out.println("Добавление товаров завершено, произведен расчет:");
+                break;
+            }
+        }
+    }
+}
+
